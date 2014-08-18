6127cc8e27eb868c8f35e22a371f8b7df42d8253
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-08-18 17:51:20 +0300

    Updated to v0.4

--- ScMvc/Properties/AssemblyInfo.cs
+++ ScMvc/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("Copyright © Pavel Nezhencev (Creuna) 2014")]
 
 // Product version
-[assembly: AssemblyVersion("0.3.4")]
-[assembly: AssemblyFileVersion("0.3.4")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
